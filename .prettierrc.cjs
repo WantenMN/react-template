722f587b3afe9cc54a684c5d88c880e42b2b8f8a
Wanten <WantenMN@gmail.com>
2023-06-12 16:27:42 +0800

    fix: update .prettier configuration to support prettier-plugin-tailwindcss

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  printWidth: 80,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  semi: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
