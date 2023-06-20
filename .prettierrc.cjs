c976383accd01603f2fe807eee705d763651bfe0
Wanten <WantenMN@gmail.com>
2023-06-20 13:34:12 +0800

    chore: replace .prettierrc.cjs with .prettierrc

--- .prettierrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSpacing: true,
-  printWidth: 80,
-  singleQuote: false,
-  tabWidth: 2,
-  trailingComma: 'es5',
-  semi: true,
-  plugins: [require('prettier-plugin-tailwindcss')],
-};
